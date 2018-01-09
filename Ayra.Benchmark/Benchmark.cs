be7c82d7f9a87e0d474d55251cba249f7e648ad1
Fusion86 <fusion86@cerbus.nl>
2018-01-09 22:58:06 +0100

    Setup benchmark project

---
+++ Ayra.Benchmark/Benchmark.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Ayra.Benchmark
+{
+    public abstract class Benchmark
+    {
+        public abstract string Name { get; }
+        public bool HasRun;
+        public virtual Benchmark[] DependsOn => new Benchmark[0];
+    }
+}
---
+++ Ayra.Benchmark/Benchmarks/DownloadTMD.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Threading;
+
+namespace Ayra.Benchmark.Benchmarks
+{
+    public class DownloadTMD : Benchmark
+    {
+        public override string Name => "Download TMD";
+
+        [Benchmark]
+        public void Run()
+        {
+            Thread.Sleep(2000);
+        }
+    }
+}
