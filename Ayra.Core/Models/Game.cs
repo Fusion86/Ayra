97ad3ff2a5ec33b18f6428e08d88d989c7e00b5e
Fusion86 <fusion86@cerbus.nl>
2018-01-14 21:51:46 +0100

    Update

---
+++ Ayra.Core/Models/Game.cs
@@ -0,0 +1,36 @@
+﻿using Ayra.Core.Classes;
+using Ayra.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayra.Core.Models
+{
+    public class Game
+    {
+        public TMD Tmd;
+        public Ticket Ticket;
+        public FST Fst;
+
+        public string LocalPath;
+
+        public async static Task<Game> GetFromNus(string titleId)
+        {
+            NUSClient client = new NUSClient(NDevice.WII_U);
+            TMD tmd = await client.DownloadTMD("0005000e101a9f00");
+
+            return new Game
+            {
+                Tmd = tmd,
+                LocalPath = Path.Combine("download", tmd.Header.TitleId.ToString("X8"))
+            };
+        }
+
+        public async void DecryptContent()
+        {
+            if (Ticket == null) throw new Exception("No ticket!");
+        }
+    }
+}
